932799c924b6bf006cf53c308dcd4382bcb5edfc
amin-egn <aminx15am@yahoo.com>
2022-05-08 16:47:50 +0430

    add go tests

---
+++ chapter10/tests.go
@@ -0,0 +1,37 @@
+package math
+
+import (
+	"testing"
+)
+
+type Testpair struct {
+	values  []float64
+	average float64
+}
+
+var tests = []Testpair{
+	{[]float64{1, 2}, 1.5},
+	{[]float64{1, 1, 1, 1, 1, 1}, 1},
+	{[]float64{-1, 1}, 0},
+}
+
+func Average(xs []float64) float64 {
+	total := float64(0)
+	for _, x := range xs {
+		total += x
+	}
+	return total / float64(len(xs))
+}
+
+func TestAverage(t *testing.T) {
+	for _, pair := range tests {
+		v := Average(pair.values)
+		if v != pair.average {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.average,
+				"got", v,
+			)
+		}
+	}
+}
